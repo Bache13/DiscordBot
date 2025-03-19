6eb9702007e7b8d003888f52ae493ac7a8aa008d
Bache13 <andrebachman13@gmail.com>
2025-03-19 12:50:30 +0100

    Fresh

--- obj/Debug/net6.0/DiscordBot.AssemblyInfo.cs
+++ obj/Debug/net6.0/DiscordBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e964ac2761193c9b7b32a8b3ed4b7e8d7c3bb079")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c64db122bd1df792eddc01929df213e1b3df7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
