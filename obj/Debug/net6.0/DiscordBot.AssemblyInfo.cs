f558d6709ab0b16f10886a2ee11a30d8fd7a9b8f
Bache13 <andrebachman13@gmail.com>
2025-03-19 13:17:05 +0100

    Struggless

--- obj/Debug/net6.0/DiscordBot.AssemblyInfo.cs
+++ obj/Debug/net6.0/DiscordBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3da5f569803bb00f5b40d983d7105af82bd2eff0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4135f9cb36d5a1b58783c1c88f20a3ade7b3358e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
